dfb4ff177bc2e9efea123b7f4a2c15fc460fd903
Abigael Amuruon <67632187+AbbyRu@users.noreply.github.com>
2026-02-19 09:45:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Highlight active page
-const links = document.querySelectorAll("nav a");
-const currentPage = window.location.pathname.split("/").pop();
-
-links.forEach(link => {
-    if(link.getAttribute("href") === currentPage){
-        link.style.textDecoration = "underline";
-    }
-});
